2293527560c5c7493342c4329643d5f785881db7
RowanLodewijks <90310170+RowanLodewijks@users.noreply.github.com>
2022-04-19 13:40:50 +0200

    Eerste normale commit

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
-<?php
 
-include './assets/database/database.php';
-
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,22 +6,24 @@ include './assets/database/database.php';
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./assets/css/styles.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="./assets/css/style.css">
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
   <title>Vrijwilligershuis donaties</title>
 </head>
 <body>
-  <div class="header"> </div>
-  <div class="container main">
-    <div class="info-1"> </div>
-    <img id=img-1 src="" ></img>
-    <div onclick="" class="button"></div>
-    <div class="info-2"> </div>
-    <img id=img-1 src="" ></img>
-  </div>
-  <div class="footer">
-    <div class="socials"></div>
-    <div class="copyright"></div>
+  <div class="container">
+    <div class="nav">
+      
+    </div>
+    <div class="header">
+      <img src="./assets/img/logo.png">
+    </div>
+    <div class="main">
+
+    </div>
+    <div class="footer">
+
+    </div>
   </div>
 </body>
 </html>
