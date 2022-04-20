03993e43404f3ce9b4c3283af364c41005f7255f
TygoVZ <210722@student.glu.nl>
2022-04-20 09:22:06 +0200

    extra files, donations aangepast

--- donaties.php
+++ donaties.php
@@ -10,7 +10,7 @@ include "./assets/database/database.php";
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./assets/css/style.css">
+  <link rel="stylesheet" href="./assets/css/styleDonations.css">
   <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
   <title>Vrijwilligershuis donaties</title>
 </head>
@@ -28,7 +28,9 @@ include "./assets/database/database.php";
       </ul>
     </nav>
     <main>
-        
+        <div id="formDonations">
+
+        </div>
     </main>
     <footer>
 
